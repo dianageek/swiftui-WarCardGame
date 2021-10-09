93e1c2f55ce41188e08b31faefa81db55824a977
dianageek <dianageek@gmail.com>
2021-10-09 12:38:32 -0500

    Initial Commit

---
+++ Lesson-5-War-challenge/Lesson_5_War_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_5_War_challengeApp.swift
+//  Lesson-5-War-challenge
+//
+//  Created by diana Ryan on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_5_War_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
